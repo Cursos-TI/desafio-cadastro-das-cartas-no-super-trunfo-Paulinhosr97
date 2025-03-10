b7137be745214d6089fe8896d8765b71f508cfdb
Paulinhosr97 <psiilva638@gmail.com>
2025-03-10 12:26:58 +0000

    commit

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -17,25 +17,28 @@ int pontos_turisticos;
 
 
 
-printf("digite populacao: \n");
+printf("digite populacao:\n");
 scanf("%d", &Populacao);
 
-printf("digite area: \n");
+printf("digite area:\n");
 scanf("%f", &area);
 
 
-printf("digite o PIB: \n");
+printf("digite o PIB:\n");
 scanf("%f", &PIB);
 
-printf("digite pontos_turisticos: \n");
+printf("digite pontos_turisticos:\n");
 scanf("%d", &pontos_turisticos);
 
-printf("populacao: %d - area: %f", Populacao ,- PIB  );
-printf("PIB: %f - pontos_turisticos2: %d, PIB, pontos_turisticos_\n \n");
+printf("\n \n");
 
-("\n \n");
+printf("populacao: %d\n area: %f\n", Populacao, area );
+printf("PIB: %f\n pontos_turisticos: %d\n", PIB, pontos_turisticos);
+
 
 // saida de dados carta 1
+
+printf("\n");
 
 printf("proxima_carta \n \n");
 
@@ -52,10 +55,12 @@ scanf("%f", &PIB);
 printf("digite pontos_turisticos: \n");
 scanf("%d", &pontos_turisticos);
 
-printf("populacao: %d - area: %f", Populacao ,- PIB  );
-printf("PIB: %f - pontos_turisticos2: %d", PIB, pontos_turisticos);
+printf("\n");
 
+printf("populacao: %d\n area: %f\n", Populacao , PIB  );
+printf("PIB: %f\n pontos_turisticos2: %d\n", PIB, pontos_turisticos);
 
+printf("\n \n");
 
 
 return 00;
